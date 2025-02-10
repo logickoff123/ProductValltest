130e6a4e6313a68547bedc318da5db4535cd4670
grin_leo <gridchin.leo@gmail.com>
2025-02-10 20:16:11 +0300

    new component

--- src/app/catalog/workshop/page.tsx
+++ src/app/catalog/workshop/page.tsx
@@ -1,8 +1,11 @@
 import WorkShopContainer from "@/containers/workshop";
+import { WorkshopProvider } from "@/context/workshop";
 
 export default function WorkShopPage() {
 
     return (
-        <WorkShopContainer />
-    )
+        <WorkshopProvider>
+            <WorkShopContainer />
+        </WorkshopProvider>
+    );
 }

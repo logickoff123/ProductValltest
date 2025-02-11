d57a016037c3c12e036eb81c2abfdb688f55941b
Hikkiray <dan4ik10005@gmail.com>
2025-02-11 04:14:15 +0300

    main pull

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import LandingPage from "./landing/page";
 export default function Home() {
   return (
     <div>
-      <LandingPage />
+        <LandingPage/>
+        <Login/>
+
     </div>
   );
 }

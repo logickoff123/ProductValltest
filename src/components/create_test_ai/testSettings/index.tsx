7ba95e34196bfd517b872890fa9cc68a86cf4fa6
Hikkiray <dan4ik10005@gmail.com>
2025-03-04 04:02:03 +0300

    without description, new button 'Перейти к тесту'

--- src/components/create_test_ai/testSettings/index.tsx
+++ src/components/create_test_ai/testSettings/index.tsx
@@ -49,21 +49,26 @@ export function TestSettings(){
 
 
     return(
-        
-        <div className="bg-secondaryBackground rounded-[24px] px-[24px] pt-[24px] pb-[24px]">
-            <h1 className="text-white font-['Arial'] font-bold text-[32px] leading-[36.8px]">Настройки теста</h1>
-            
-            <div className="mt-[15px] space-x-4 flex-row flex">
-                <TextPrompt label="Название Теста" placeholder="Напишите название теста" value={testName} onChange={(e) => setTestName(e.target.value)} /> {/* Подключаем textarea */}
-                <TextPrompt label="Количество вопросов" placeholder="Напишите кол-во вопросов" value={numberOfQuestions} onChange={(e) => setNumberOfQuestions(e.target.value)} />
-            </div>
+        <div className="flex flex-col gap-[24px]">
+            <div className="bg-secondaryBackground rounded-[24px] px-[24px] pt-[24px] pb-[24px]">
+                <h1 className="text-white font-['Arial'] font-bold text-[32px] leading-[36.8px]">Настройки теста</h1>
+                
+                <div className="mt-[15px] space-x-4 flex-row flex">
+                    <TextPrompt label="Название Теста" placeholder="Напишите название теста" value={testName} onChange={(e) => setTestName(e.target.value)} /> {/* Подключаем textarea */}
+                    <TextPrompt label="Количество вопросов" placeholder="Напишите кол-во вопросов" value={numberOfQuestions} onChange={(e) => setNumberOfQuestions(e.target.value)} />
+                </div>
 
-            <div className="flex space-x-4 mt-[16px]">
-                <Dropdown id="dropdown4" label="Время" placeholder="Время на одно задание" options={times} selected={time} onChange={setTime} />
-                <Dropdown id="dropdown5" label="Количество баллов" placeholder="Кол-во баллов за 1 задание" options={numberOfPointss} selected={numberOfPoints} onChange={setNumberOfPoints} />
-                <Dropdown id="dropdown6" label="Статус" placeholder="Выберите статус теста" options={statuss} selected={status} onChange={setStatus} />
+                <div className="flex space-x-4 mt-[16px]">
+                    <Dropdown id="dropdown4" label="Время" placeholder="Время на одно задание" options={times} selected={time} onChange={setTime} />
+                    <Dropdown id="dropdown5" label="Количество баллов" placeholder="Кол-во баллов за 1 задание" options={numberOfPointss} selected={numberOfPoints} onChange={setNumberOfPoints} />
+                    <Dropdown id="dropdown6" label="Статус" placeholder="Выберите статус теста" options={statuss} selected={status} onChange={setStatus} />
+                </div>
             </div>
-
+            <div>
+                <Link href="" className="flex bg-[rgba(193,239,0,1)] text-black font-sans font-normal text-[16px] leading-[18.4px] rounded-[12px] px-[12px] w-fit h-[42px] justify-center items-center">
+                    Перейти к тесту
+                </Link>
+            </div>
         </div>
         
     )
--- src/components/create_test_ai/textPrompt/index.tsx
+++ src/components/create_test_ai/textPrompt/index.tsx
@@ -19,7 +19,7 @@ const TextPrompt: React.FC<TextPromptProps> = ({ value, label, placeholder, isDe
       
       <textarea
         className={`py-[14px] px-[16px] text-white outline-none outline-[1px] focus:outline-[rgba(193,239,0,1)] resize-none placeholder-[rgba(73,73,74,1)] bg-mainBackground ${
-          isDescription? 'h-[119px] w-full' : 'flex h-[42px] w-[291px] py-[12px] items-center cursor-pointer text-[rgba(73,73,74,1)] font-[Arial] font-normal text-[16px] leading-[18.4px] rounded-[12px]'
+          isDescription? 'h-[119px] w-full' : 'flex h-[42px] w-[291px] py-[11px] items-center cursor-pointer text-[rgba(73,73,74,1)] font-[Arial] font-normal text-[16px] leading-[18.4px] rounded-[12px]'
         } rounded-[12px]`}
 
         placeholder={placeholder}

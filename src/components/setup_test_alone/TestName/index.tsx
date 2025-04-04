45ee4f935a64eb8749674c6a1c4082724caf6c5a
Daria <haragozan@gmail.com>
2025-04-04 18:26:23 +0300

    Добавление TestName сразу в контейнер setup_test_alone

--- src/components/setup_test_alone/TestName/index.tsx
+++ src/components/setup_test_alone/TestName/index.tsx
@@ -1,9 +1,9 @@
-'use client';
-
-export const TestName = ({ name }: { name: string }) => {
-  return (
-    <div className="bg-[#141415] p-6 rounded-[24px] shadow-md mb-6 flex items-center">
-      <h1 className="text-3xl text-white font-bold">{name}</h1>
-    </div>
-  );
-};
+//'use client';
+//
+//export const TestName = ({ name }: { name: string }) => {
+//  return (
+//    <div className="bg-[#141415] p-6 rounded-[24px] shadow-md mb-6 flex items-center">
+//      <h1 className="text-3xl text-white font-bold">{name}</h1>
+//    </div>
+//  );
+//};
